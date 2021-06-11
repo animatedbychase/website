e6bc7126a2a571b3a129727ce75509f7ab37ddf4
animatedbychase <85714749+animatedbychase@users.noreply.github.com>
2021-06-10 19:09:58 -0500

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,22 @@
+
+const previews = document.querySelectorAll(".gallery img")
+var modal = document.getElementById("myModal");
+const original = document.querySelector(".modal-content")
+var modalImg = document.getElementById("img01")
+
+
+previews.forEach(preview => {
+    preview.addEventListener('click', () => {
+        modal.style.display = "block";
+        const originalSrc = preview.getAttribute('data-original');
+        console.log(originalSrc)
+        original.src = `img/${originalSrc}`;
+    })
+})
+
+
+window.onclick = function(event)  {
+    if (event.target == modal) {
+        modal.style.display = "none";
+      }
+}
