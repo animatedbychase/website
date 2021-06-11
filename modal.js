17d319d270f4226b40f9493ae299d5e81368314b
animatedbychase <85714749+animatedbychase@users.noreply.github.com>
2021-06-10 21:57:15 -0500

    Update modal.js

--- modal.js
+++ modal.js
@@ -15,7 +15,7 @@ previews.forEach(preview => {
 })
 
 let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
-modal.addEventListener('touchstart', (event) => {
+modal.addEventListener(touchEvent, (event) => {
     if (event.target == modal) {
         modal.style.display = "none";
       }
